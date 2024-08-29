f5fbb27b78ab0a9f9159a85bfd69b2f2cd3148d6
AKASH <87299311+gamkers@users.noreply.github.com>
2024-08-29 09:55:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,11 @@ def main():
                     elif str(type(result)) == "<class 'str'>":
                         st.write(result)
                     elif str(type(result)) == "<class 'numpy.int64'>":
+                        
                         st.write(result)
+                    elif "/mount/src/csv_rag/exports/charts/temp_chart.png":
+                        st.image(result, use_column_width=True)
+                    
                     else:
                         st.image(result, use_column_width=True)
                 else:
